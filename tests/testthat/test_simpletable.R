a9e5637a7acef8fa5a993014a5dcd8c1dc4b947c
David Lawrence Miller <github@ninepointeightone.net>
2014-01-06 10:52:04 -0500

    commented out extra region simple table stuff for now

--- tests/testthat/test_simpletable.R
+++ tests/testthat/test_simpletable.R
@@ -1,27 +1,28 @@
 library(Distance)
 library(mrds)
 
-context("Testing simplified data tables")
+context("Testing simplified data tables (tee data)")
+
+## first need to take the golf tee data
+data(book.tee.data)
+region <- book.tee.data$book.tee.region
+samples <- book.tee.data$book.tee.samples
+obs <- book.tee.data$book.tee.obs
+obs <- obs[order(obs$object),]
+egdata <- book.tee.data$book.tee.dataframe
+# take only observer 1 data
+egdata <- egdata[egdata$observer==1,]
+
+# merge sample onto region
+flatdat <- merge(region,samples,by="Region.Label")
+# merge obs table onto that
+flatdat <- merge(flatdat,obs,by=c("Sample.Label","Region.Label"),all.x=TRUE)
+# finally merge the distances onto that
+flatdat <- merge(flatdat,egdata,by="object",all.x=TRUE)
+
 
 
 test_that("Recover structure for tee data",{
-
-  ## first need to take the golf tee data and put it into a flat format
-  data(book.tee.data)
-  region <- book.tee.data$book.tee.region
-  samples <- book.tee.data$book.tee.samples
-  obs <- book.tee.data$book.tee.obs
-  obs <- obs[order(obs$object),]
-  egdata <- book.tee.data$book.tee.dataframe
-  # take only observer 1 data
-  egdata <- egdata[egdata$observer==1,]
-
-  # merge sample onto region
-  flatdat <- merge(region,samples,by="Region.Label")
-  # merge obs table onto that
-  flatdat <- merge(flatdat,obs,by=c("Sample.Label","Region.Label"),all.x=TRUE)
-  # finally merge the distances onto that
-  flatdat <- merge(flatdat,egdata,by="object",all.x=TRUE)
 
 
   ## check we can recover the 4 data frames
@@ -38,3 +39,14 @@ test_that("Recover structure for tee data",{
 
 })
 
+#test_that("Extra regions",{
+#
+#  # add an extra region
+#  region.e <- data.frame(Region.Label=1:3,Area=c(1040,640,500))
+#  samples
+#
+#  # estimate using all tables
+#  dsmod.e <- ds(egdata,4,region.table=region.e, sample.table=samples,
+#                obs.table=obs)
+#
+#}
