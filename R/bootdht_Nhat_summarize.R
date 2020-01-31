b8087ba020d0514f62792025b99c5a4bb1acdbcc
David Lawrence Miller <github@ninepointeightone.net>
2020-01-31 11:51:47 +0000

    add link to examples site

--- R/bootdht_Nhat_summarize.R
+++ R/bootdht_Nhat_summarize.R
@@ -1,6 +1,7 @@
 #' Simple summary for bootstrap model
 #'
 #' When using \code{\link{bootdht}} one needs to use a summary function to extract results from the resulting models per replicate. This function is the simplest possible example of such a function, that just extracts the estimated abundance.
+#' Further examples of such functions can be found at \url{http://examples.distancesampling.org}.
 #'
 #' @param ests output from \code{\link{dht2}}.
 #' @param fit fitted detection function object (unused).
