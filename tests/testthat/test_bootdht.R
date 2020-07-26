dcebf1f08399d00ebc5a38361c2535198104a65f
David Lawrence Miller <github@ninepointeightone.net>
2020-07-26 09:10:54 +0100

    ...

--- tests/testthat/test_bootdht.R
+++ tests/testthat/test_bootdht.R
@@ -6,7 +6,8 @@ test_that("area=0 with default summary",{
   amakihi <- amakihi[1:300,]
   conv <- convert_units("meter", NULL, "hectare")
   surv <- ds(amakihi, transect="point", key="hr",
-             formula=~Region.Label, convert.units=conv, truncation = 82.5)
+             formula=~Region.Label, convert.units=conv,
+             truncation = 82.5, er.var="P3")
   expect_error(bootdht(surv, flatfile=amakihi, nboot=5),
                "No Area in flatfile, densities will be returned and the default summary function records only abundances. You need to write your own summary_fun.")
 
@@ -15,7 +16,5 @@ test_that("area=0 with default summary",{
                "No Area in flatfile, densities will be returned and the default summary function records only abundances. You need to write your own summary_fun.")
 
 })
-
-
 
 
