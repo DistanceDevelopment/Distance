26a3bdd72049f8cb233ccb5ae548db5d98d72378
David Lawrence Miller <github@ninepointeightone.net>
2020-07-29 15:22:14 +0100

    remove dead comments

--- R/varNhat.R
+++ R/varNhat.R
@@ -1,7 +1,7 @@
 # this code is HORRIBLE
 # Calculate the variance contribution of the detection function
 #  to abundance estimates
-varNhat <- function(data, model, strata){
+varNhat <- function(data, model){
 
   # format the data
   # definitely a bad idea, relies on dplyr's internal representation
@@ -26,10 +26,6 @@ varNhat <- function(data, model, strata){
 
   }
 
-  # remove data where we don't have observations, since this doesn't effect
-  # the predictions...
-  #data <- data[!is.na(data$object), ]
-
   # function to calculate Nhat
   dhtd <- function(par, data, model, area, covered_area, ind){
     # result store
@@ -50,28 +46,6 @@ varNhat <- function(data, model, strata){
     res
   }
 
-#  # function to calculate P_a
-#  phtd <- function(par, data, model, ind){
-#    # result store
-#    res <- rep(NA, length(ind))
-#
-#    # set par
-#    model$par <- par
-#
-#    # calculate Nc
-#    data$p <- predict(model, newdata=data, integrate=TRUE, compute=TRUE)$fitted
-#    data$Nc <- 1/data$p
-#
-#    # calculate Nhat per region
-#    for(i in 1:length(ind)){
-#      # p = n/Nhat
-#      res[i] <- sum(data$n_observations[ind[[i]]], na.rm=TRUE)/
-#                 sum(data$Nc[ind[[i]]], na.rm=TRUE)
-#    }
-#    res
-#  }
-
-
   # get variance-covariance matrix for the detection function
   vcov <- solvecov(model$hessian)$inv
 
@@ -80,18 +54,8 @@ varNhat <- function(data, model, strata){
                     model=model, data=data, area=area,
                     covered_area=covered_area, ind=ind)
   attr(dm, "vardat_str") <- vardat_str
-#  # calculate variance
-#  pm <- DeltaMethod(model$par, phtd, vcov, sqrt(.Machine$double.eps),
-#                    model=model, data=data, ind=ind)
-#
-#  covar <- t(dm$partial) %*% vcov %*% pm$partial + pm$covar
-#  average.p <- phtd(model$par, data, model, ind)
-#  Nhat <- dhtd(model$par, data, model, area=area, covered_area=covered_area, ind)
-#  var.pbar <- average.p^2*((sqrt(dm$variance)/Nhat)^2 + pm$var/data$n_observations^2-
-#                                  2*covar/(data$n_observations*Nhat))
-#
 
-  ret <- list(Nhat=dm)#, phat=var.pbar)
+  ret <- list(Nhat=dm)
 
   attr(ret, "vardat_str") <- vardat_str
   return(ret)
