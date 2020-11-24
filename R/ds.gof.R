1a9e831aa831a4f5e8dce55a5c3508c69bc3b213
David Lawrence Miller <github@ninepointeightone.net>
2020-11-24 14:55:43 +0000

    deprecate ds.gof, move to gof_ds, add documentation

--- R/ds.gof.R
+++ R/ds.gof.R
@@ -1,28 +1,19 @@
 #' Goodness of fit tests for distance sampling models
 #'
-#' Chi-square, Kolmogorov-Smirnov (if \code{ks=TRUE}) and Cramer-von Mises goodness of fit tests for detection function models.
+#' This function is deprecated, please see \code{link{gof_ds}}.
 #'
 #' @export
-#' @param model fitted model object
-#' @param breaks Cutpoints to use for binning data
-#' @param nc Number of distance classes
-#' @param qq Flag to indicate whether quantile-quantile plot is desired
-#' @param ks perform the Kolmogorov-Smirnov test (this involves many bootstraps so can take a while)
-#' @param \dots Graphics parameters to pass into qqplot function
-#' @return List of test results and a plot.
+#' @param model deprecated.
+#' @param breaks deprecated.
+#' @param nc deprecated.
+#' @param qq deprecated.
+#' @param ks deprecated.
+#' @param \dots deprecated.
+#' @return Nothing, deprecated.
 #' @author David L Miller
 #' @seealso qqplot.ddf ddf.gof
 #' @keywords utility
 #' @examples
-#' \dontrun{
-#' # fit and test a simple model for the golf tee data
-#' library(Distance)
-#' data(book.tee.data)
-#' tee.data<-book.tee.data$book.tee.dataframe[book.tee.data$book.tee.dataframe$observer==1,]
-#' ds.model <- ds(tee.data,4)
-#' ds.gof(ds.model)
-#'}
 ds.gof <- function(model, breaks=NULL, nc=NULL, qq=TRUE, ks=FALSE, ...){
-  return(suppressMessages(ddf.gof(model$ddf, breaks=breaks, nc=nc, qq=qq, ks=ks,
-                                 ...)))
+  stop("ds.gof is deprected, please use gof_ds")
 }
--- R/gof_ds.R
+++ R/gof_ds.R
@@ -1,11 +1,15 @@
 #' Goodness of fit testing and quantile-quantile plots
 #'
-#' Formal goodness of fit testing for detection function models using Kolmogorov-Smirnov and Cramer-von Mises tests. Both tests are based on looking at the quantile-quantile plot produced by \code{\link{qqplot.ddf}} and deviations from the line x=y.
+#' Goodness of fit testing for detection function models. For continuous distances Kolmogorov-Smirnov and Cramer-von Mises tests can be used, when binned or continuous distances are used a chi-squared test can be used.
+#'
+#' Kolmogorov-Smirnov and Cramer-von Mises tests are based on looking at the quantile-quantile plot produced by \code{\link{qqplot.ddf}} and deviations from the line x=y.
 #'
 #' The Kolmogorov-Smirnov test asks the question "what's the largest vertical distance between a point and the y=x line?" It uses this distance as a statistic to test the null hypothesis that the samples (EDF and CDF in our case) are from the same distribution (and hence our model fits well). If the deviation between the y=x line and the points is too large we reject the null hypothesis and say the model doesn't have a good fit.
 #'
 #' Rather than looking at the single biggest difference between the y=x line and the points in the Q-Q plot, we might prefer to think about all the differences between line and points, since there may be many smaller differences that we want to take into account rather than looking for one large deviation. Its null hypothesis is the same, but the statistic it uses is the sum of the deviations from each of the point to the line.
-#
+#'
+#' A chi-squared test is also run if \code{chisq=TRUE}. In this case binning of distances is required if distance data are continuous. This can be specified as a number of equally-spaced bins (using the argument \code{nc=}) or the cutpoints of bins (using \code{breaks=}). The test compares the number of observations in a given bin to the number predicted under the fitted detection function.
+#'
 #' @section Details:
 #'
 #' Note that a bootstrap procedure is required for the Kolmogorov-Smirnov test to ensure that the p-values from the procedure are correct as the we are comparing the cumulative distribution function (CDF) and empirical distribution function (EDF) and we have estimated the parameters of the detection function. The \code{nboot} parameter controls the number of bootstraps to use. Set to \code{0} to avoid computing bootstraps (much faster but with no Kolmogorov-Smirnov results, of course).
@@ -15,6 +19,8 @@
 #' @param chisq if \code{TRUE} then chi-squared statistic is calculated even for models that use exact distances. Ignored for models that use binned distances
 #' @param nboot number of replicates to use to calculate p-values for the Kolmogorov-Smirnov goodness of fit test statistics
 #' @param ks perform the Kolmogorov-Smirnov test (this involves many bootstraps so can take a while)
+#' @param nc number of evenly-spaced distance classes for chi-squared test, if \code{chisq=TRUE}
+#' @param breaks cutpoints to use for binning, if \code{chisq=TRUE}
 #' @param ... other arguments to be passed to \code{\link{ddf.gof}}
 #' @export
 #' @examples
@@ -27,9 +33,11 @@
 #' # don't make plot
 #' gof_ds(ds.model, plot=FALSE)
 #'}
-gof_ds <- function(model, plot=TRUE, chisq=FALSE, nboot=100, ks=FALSE, ...){
+gof_ds <- function(model, plot=TRUE, chisq=FALSE, nboot=100, ks=FALSE, nc=NULL,
+                   ...){
 
-  gof <- suppressMessages(ddf.gof(model$ddf, qq=plot, nboot=nboot, ks=ks, ...))
+  gof <- suppressMessages(ddf.gof(model$ddf, qq=plot, nboot=nboot, ks=ks, nc=nc,
+                                  ...))
 
   if(model$ddf$meta.data$binned | chisq){
     return(gof)
