2ec0e07a4704c61bf112cb2a1499e8fc34afc098
David Lawrence Miller <github@ninepointeightone.net>
2021-06-14 12:08:53 +0100

    remove multicombine

--- R/bootdht.R
+++ R/bootdht.R
@@ -267,8 +267,7 @@ bootdht <- function(model,
     # fit the model nboot times over cores cores
     # note there is a bit of fiddling here with the progress bar to get it to
     # work (updates happen in this loop rather than in bootit)
-    boot_ests <- foreach::foreach(i=1:nboot,
-                                  .multicombine=TRUE) %dopar2% {
+    boot_ests <- foreach::foreach(i=1:nboot) %dopar2% {
       r <- bootit(dat, models=models, our_resamples=our_resamples,
                   summary_fun=summary_fun, convert.units=convert.units,
                   pb=list(increment=function(pb){invisible()}))
