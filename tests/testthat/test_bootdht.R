c76c26786f41016d1222ea9ee766d72e39e44237
David Lawrence Miller <github@ninepointeightone.net>
2021-08-16 15:38:11 +0100

    why not test that?

--- tests/testthat/test_bootdht.R
+++ tests/testthat/test_bootdht.R
@@ -21,3 +21,64 @@ test_that("area=0 with default summary",{
 })
 
 
+# generate some data to test on
+dat <- data.frame(Sample.Label = 1:10)
+dat$Region.Label <- c(rep(1, 5), rep(2, 5))
+set.seed(123)
+dat2 <- dat[sample(1:nrow(dat), nrow(dat), replace=TRUE), ]
+dat2$object <- 1:nrow(dat2)
+dat <- dat[!(dat$Sample.Label %in% dat2$Sample.Label),]
+dat$object <- NA
+dat <- rbind(dat, dat2)
+dat$save.ind <- 1:nrow(dat)
+
+
+test_that("resamples work - strata", {
+
+  set.seed(123)
+  obs <- bootdht_resample_data(dat, c("Region.Label"))
+
+  expect_equal(obs$save.ind, c(1, 4, 5, 7, 9, 10, 2, 3, 6, 13, 8, 11, 12))
+})
+
+
+test_that("resamples work - sample", {
+
+  set.seed(123)
+  obs <- bootdht_resample_data(dat, c("Sample.Label"))
+
+  expect_equal(obs$save.ind, c(7, 7, 4, 5, 4, 5, 7, 12, 8, 11, 2, 3, 6, 13))
+})
+
+test_that("resamples work - object", {
+
+  set.seed(123)
+  obs <- bootdht_resample_data(dat, c("object"))
+
+  expect_equal(obs$save.ind, c(1, 4, 5, 6, 5, 2, 3, 6, 11, 9))
+})
+
+test_that("resamples work - strata/object", {
+
+  set.seed(123)
+  obs <- bootdht_resample_data(dat, c("Region.Label", "object"))
+
+  expect_equal(obs$save.ind, c(1, 4, 5, 5, 5, 9, 2, 3, 13, 8, 11, 8))
+})
+
+
+test_that("resamples work - sample/object", {
+
+  set.seed(123)
+  obs <- bootdht_resample_data(dat, c("Sample.Label", "object"))
+
+  expect_equal(obs$save.ind, c(7, 4, 4, 4, 5, 4, 12, 8, 2, 3, 6, 13))
+})
+
+test_that("resamples work - strata/sample/object", {
+
+  set.seed(1223)
+  obs <- bootdht_resample_data(dat, c("Region.Label", "Sample.Label", "object"))
+
+  expect_equal(obs$save.ind, c(6, 1, 9, 6, 6, 2, 3, 8, 11, 6, 13, 8))
+})
