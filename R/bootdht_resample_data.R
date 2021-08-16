eab2dfc26c0958c7cc85ac0747804eb28d6ae757
David Lawrence Miller <github@ninepointeightone.net>
2021-08-16 14:13:20 +0100

    re-write bootdht_resample_data to properly account for transects with no observations
    fixes #42

--- R/bootdht_resample_data.R
+++ R/bootdht_resample_data.R
@@ -3,55 +3,69 @@ bootdht_resample_data <- function(bootdat, our_resamples,
                                   sample_label="Sample.Label",
                                   obs_label="object"){
 
-  # what are the unique combinations
-  bootdat_samps <- unique(bootdat[, c(stratum_label, sample_label, obs_label)])
+  # unique combinations
+  bf <- unique(bootdat[, c(stratum_label, sample_label, obs_label)])
 
+  # stratum resample
   # which strata?
-  strata <- unique(bootdat_samps[[stratum_label]])
+  strata <- unique(bootdat[[stratum_label]])
   # resample if that's what we're doing, else we keep all strata
   if(stratum_label %in% our_resamples){
     strata <- sample(strata, length(strata), replace=TRUE)
   }
+  strata_df <- cbind(as.character(strata), make.unique(as.character(strata)))
+  colnames(strata_df) <- c(stratum_label, ".new_stratum_label")
 
-  # which samples?
   # get all samples per stratum
-  samps_per_strata <- by(bootdat_samps, bootdat_samps$Region.Label,
-                         function(x) unique(x$Sample.Label))
-  # restrict to those selected above (so there may be repeats of the
-  # stratum name here, don't use names(samps_per_strata)!
-  samps_per_strata <- samps_per_strata[strata]
+  samps_per_strata <- unique(bf[, c(stratum_label, sample_label)])
+  samps_per_strata <- by(bf[,c(stratum_label, sample_label)],
+                         bf[[stratum_label]],
+                         function(x) unique(x[[sample_label]]))
+
 
   # resample if that's what we're doing, else we keep all samples
-  samples <- unlist(samps_per_strata)
+  samples <- samps_per_strata
   if(sample_label %in% our_resamples){
-    samples <- unlist(lapply(samps_per_strata, function(x){
+    samples <- lapply(samps_per_strata, function(x){
       sample(x, length(x), replace=TRUE)
-    }))
+    })
   }
 
   # get observations for each transect
-  obs_per_sample <- by(bootdat_samps, bootdat_samps$Sample.Label,
-                       function(x) unique(x$object))
+  obs_per_sample <- by(bf[,c(sample_label, obs_label)],
+                       bf[[sample_label]],
+                       function(x) unique(x[[obs_label]]))
   # restrict to those selected above
-  obs_per_sample <- obs_per_sample[samples]
+  obs_per_sample <- obs_per_sample[unlist(samples)]
 
   # resample if that's what we're doing, else we keep all observations
-  obs <- unlist(obs_per_sample)
+  obs <- obs_per_sample
   if(obs_label %in% our_resamples){
-    obs <- unlist(lapply(obs_per_sample, function(x){
+    obs <- lapply(obs_per_sample, function(x){
       sample(x, length(x), replace=TRUE)
-    }))
+    })
   }
 
-  # get all the observations (this will include NAs)
-  #this_resample <- bootdat[bootdat$object %in% obs, ]
-  this_resample <- bootdat[rep(1, length(obs)), ]
-  for(i in 1:nrow(this_resample)){
-    this_resample[i, ] <- bootdat[bootdat$object == obs[i], ]
+  # remap sample labels
+  remap <- make.unique(names(obs))
+
+  # now populate each list element with the corresponding observations
+  # or empty transects
+  for(ii in 1:length(obs)){
+    if(all(is.na(obs[[ii]]))){
+      obs[[ii]] <- bootdat[bootdat[[sample_label]] == names(obs)[ii], ]
+    }else{
+      obs[[ii]] <- bootdat[bootdat$object %in% obs[[ii]], ]
+    }
+    obs[[ii]][[sample_label]] <- remap[ii]
   }
 
-  # get the object names to be unique
-  this_resample[[obs_label]] <- 1:nrow(this_resample)
+  # concatenate list elements to data.frame
+  rr <- do.call(rbind.data.frame, obs)
 
-  return(this_resample)
+  # reset the object IDs to be unique (where there are observations)
+  rr[[obs_label]][is.na(rr[["distance"]])] <- NA
+  rr[[obs_label]][!is.na(rr[["distance"]])] <- 1:length(rr[[obs_label]][!is.na(rr[["distance"]])])
+
+  return(rr)
 }
