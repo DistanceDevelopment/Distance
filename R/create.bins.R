a63b36ff729b155e1ecfb26367e4c1f6da0e152a
David Lawrence Miller <github@ninepointeightone.net>
2022-03-16 11:44:00 +0000

    fix misc links

--- R/create.bins.R
+++ R/create.bins.R
@@ -1,6 +1,6 @@
 #' Create bins from a set of binned distances and a set of cutpoints.
 #'
-#' `create.bins` is now deprecated, please use [`create_bins`](create_bins)
+#' `create.bins` is now deprecated, please use [`create_bins`][create_bins]
 #'
 #' @param data `data.frame` with at least the column `distance`.
 #' @param cutpoints vector of cutpoints for the bins
